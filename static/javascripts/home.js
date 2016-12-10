dab35d9170e34489d50ac8217acbc8e75c54ed8f
afterloe <605728727@qq.com>
2016-12-10 15:32:31 +0800

    完成左边详情选配页

--- static/javascripts/home.js
+++ static/javascripts/home.js
@@ -9,18 +9,6 @@
  */
 "use strict";
 
-// import {choseApp, showApp} from '/javascripts/yunxiComponent.js'
-
-class ChoseApp_Info extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (<div>i'm afterloe</div>);
-	}
-}
-
 class ChoseApp extends React.Component {
 	constructor(props) {
 		super(props);
@@ -29,7 +17,7 @@ class ChoseApp extends React.Component {
 
 	chose(event) {
 		const choseStyleId  = event['currentTarget'].getAttribute('data-id');
-		this['props'].onChose(choseStyleId);
+		this['props'].onChose(choseStyleId, this['props']['isInfo']);
 	}
 
 	renderStyles() {
@@ -46,9 +34,48 @@ class ChoseApp extends React.Component {
 		));
 	}
 
+	renderStyleInfo() {
+		const {btn_text, text_align, isInfo} = this['props'];
+		return (
+			<div className='col-md-5 choiceJeaketApp_border'>
+				<div className='row'>
+						<span className='pull-left backChoseStyle'><span className='backIcon'></span>返回选择上衣 </span>
+						<span className='pull-right btn_customization'>没有喜欢的?<span className='pull-right customizationIcon'></span></span>
+				</div>
+				<div className='row'>
+						<span className='styleFullName'>CARNES 上衣</span>
+						<small className='styleInfo'>建议零售价: 10k~15K</small>
+						<small className='styleInfo'>注意：最大体重不得超过80kg</small>
+				</div>
+				<div className='row'>
+						<span className='styleChose'>选择颜色</span>
+						<div className='col-md-3 infoItem'>
+								<img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg' className='img-responsive center-block infoImage' />
+								<p>NAVY</p>
+						</div>
+						<div className='col-md-3 infoItem'>
+								<img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg' className='img-responsive center-block infoImage'  />
+								<p>NAVY</p>
+						</div>
+						<div className='col-md-3 infoItem'>
+								<img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg' className='img-responsive center-block infoImage'  />
+								<p>NAVY</p>
+						</div>
+						<div className='col-md-3 infoItem'>
+								<img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg' className='img-responsive center-block infoImage' />
+								<p>NAVY</p>
+						</div>
+				</div>
+				<div className='btn_styleInfo'>
+						查看详情
+				</div>
+			</div>
+		);
+	}
+
 	render() {
 		const {btn_text, text_align, isInfo} = this['props'];
-		console.log(isInfo);
+		if (isInfo) return this.renderStyleInfo();
 		const styles = this.renderStyles();
 		return (
 			<div className='col-md-5 choiceJeaketApp_border'>
@@ -64,12 +91,23 @@ class ShowApp extends React.Component {
 		super(props);
 	}
 
+	renderSaveButton() {
+		const flag = this['props']['showSave'];
+		console.log(flag);
+		if (!flag) return;
+		return (
+			<span className='btn_saveChose'>保存方案</span>
+		);
+	}
+
 	render() {
+		const saveButton = this.renderSaveButton();
 		return (
 			<div className='col-md-2'>
 				<img className='jacket' src={'/images/warehouse/' + this['props']['jacket']} />
 				<img className='pants' src={'/images/warehouse/' + this['props']['pants']} />
 				<img className='shadow' src='/images/warehouse/bottom-shadow.png' />
+				{saveButton}
 			</div>
 		);
 	}
@@ -78,11 +116,13 @@ class ShowApp extends React.Component {
 ChoseApp['defaultProps'] = {
 	items: [],
 	btn_text: '默认标题',
+	isInfo: true,
 	text_align: 'left'
 }
 
 ShowApp['defaultProps'] = {
 	jacket: 'default-men-top.png',
+	showSave: true,
 	pants: 'default-men-bottom.png'
 }
 
@@ -124,19 +164,21 @@ class SeletedApp extends React.Component {
 		this['chosePants'] = this['chosePants'].bind(this);
 	}
 
-	choseJacket(id) {
+	choseJacket(id,isInfo) {
 		const choseJacke = this['state']['jacketItems'][id];
+		if(!isInfo) isInfo = true;
 		this.setState({
 			choseJacket: choseJacke['representative'],
-			goToJacketInfo: true
+			goToJacketInfo: isInfo
 		});
 	}
 
 	chosePants(id, isInfo) {
 		const chosePants = this['state']['pantsItems'][id];
+		if(!isInfo) isInfo = true;
 		this.setState({
 			chosePants: chosePants['representative'],
-			goToPantsInfo: true
+			goToPantsInfo: isInfo
 		});
 	}
 
@@ -145,7 +187,7 @@ class SeletedApp extends React.Component {
 		return (
 			<div className="row">
 				<ChoseApp items={jacketItems} btn_text="选择上衣" text_align="right" onChose={this.choseJacket} isInfo={goToJacketInfo}/>
-				<ShowApp jacket={choseJacket} pants={chosePants}/>
+				<ShowApp jacket={choseJacket} pants={chosePants} showSave={goToJacketInfo && goToPantsInfo}/>
 				<ChoseApp items={pantsItems} btn_text="选择裤子" text_align="left" onChose={this.chosePants} isInfo={goToPantsInfo}/>
 				<ContrastBar />
 			</div>
