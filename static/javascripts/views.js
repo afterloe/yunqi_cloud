59b6d8e0021427ecc9c2055d5c52112f053add54
afterloe <605728727@qq.com>
2016-12-12 23:26:20 +0800

    产品视图前端数据激活，等待后端传递数据

--- static/javascripts/views.js
+++ static/javascripts/views.js
@@ -9,81 +9,82 @@
  */
 "use strict";
 
+const loadHotAllocation = activityView => {
+      if ('热点定制项' !== activityView) return [];
+      return [1,2];
+}
+
+const loadHotSell = activityView => {
+      if ('最热销' !== activityView) return loadHotAllocation(activityView);
+      return [1,2,3,4,5];
+}
+
+const loadHightClick = activityView => {
+      if ('最吸引眼球' !== activityView) return loadHotSell(activityView);
+      return [1,2,3];
+};
+
 class SellViews extends React.Component {
     constructor(props) {
         super(props);
+        this['changeSelectView'] = this['changeSelectView'].bind(this);
+        this['state'] = {
+          activityView: props['views'][0]
+        };
+    }
+
+    componentWillMount() {
+        const {activityView} = this['state'];
+        const items = loadHightClick(activityView);
+        this.setState({items});
+    }
+
+    changeSelectView(event) {
+        const activityView = event['currentTarget']['innerText'];
+        const items = loadHightClick(activityView);
+        this.setState({activityView, items});
+    }
+
+    renderViewItems() {
+        const {items} = this['state'];
+        return items.map(item => (
+          <div className='col-md-3 sellViews-item'>
+              <div className='sellViews-view'><img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg'/></div>
+              <div className='sellViews-card'>
+                  <p className='sellViews-pri'>价格 : $225.00</p>
+                  <p className='sellViews-info'>商品名 : REGULAR</p>
+                  <p className='sellViews-info'>色系 : PRIMALOFT SILVER</p>
+                  <div className='sellViews-other'>
+                    <span className='pull-left'>库存: 219</span>
+                    <span className='pull-right'>交货周期: 3~5 天</span>
+                  </div>
+              </div>
+          </div>
+        ));
+    }
+
+    renderViewsList() {
+        const {activityView} = this['state'];
+        return this['props']['views'].map(view => (<span className={view === activityView? 'compareActive':''} onClick={this.changeSelectView}>{view}</span>));
     }
 
     render() {
       return (
         <div className='container'>
-            <div className='pull-right sellViews-addView'>+ 添加视图</div>
-              <div className='row compareKeys'>
-                  <span className='compareActive'>最吸引眼球</span>
-                  <span>最热销</span>
-                  <span>热点定制项</span>
-              </div>
-              <div className='row compareValues'>
-
-                  <div className='col-md-3 sellViews-item'>
-                      <div className='sellViews-view'><img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg'/></div>
-                      <div className='sellViews-card'>
-                          <p className='sellViews-pri'>价格 : $225.00</p>
-                          <p className='sellViews-info'>商品名 : REGULAR</p>
-                          <p className='sellViews-info'>色系 : PRIMALOFT SILVER</p>
-                          <div className='sellViews-other'>
-                            <span className='pull-left'>库存: 219</span>
-                            <span className='pull-right'>交货周期: 3~5 天</span>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div className='col-md-3 sellViews-item'>
-                      <div className='sellViews-view'><img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg'/></div>
-                      <div className='sellViews-card'>
-                          <p className='sellViews-pri'>价格 : $225.00</p>
-                          <p className='sellViews-info'>商品名 : REGULAR</p>
-                          <p className='sellViews-info'>色系 : PRIMALOFT SILVER</p>
-                          <div className='sellViews-other'>
-                            <span className='pull-left'>库存: 219</span>
-                            <span className='pull-right'>交货周期: 3~5 天</span>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div className='col-md-3 sellViews-item'>
-                      <div className='sellViews-view'><img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg'/></div>
-                      <div className='sellViews-card'>
-                          <p className='sellViews-pri'>价格 : $225.00</p>
-                          <p className='sellViews-info'>商品名 : REGULAR</p>
-                          <p className='sellViews-info'>色系 : PRIMALOFT SILVER</p>
-                          <div className='sellViews-other'>
-                            <span className='pull-left'>库存: 219</span>
-                            <span className='pull-right'>交货周期: 3~5 天</span>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div className='col-md-3 sellViews-item'>
-                      <div className='sellViews-view'><img src='/images/img-9a76980c0bc640b9bcb25f50fecef09b.jpg'/></div>
-                      <div className='sellViews-card'>
-                          <p className='sellViews-pri'>价格 : $225.00</p>
-                          <p className='sellViews-info'>商品名 : REGULAR</p>
-                          <p className='sellViews-info'>色系 : PRIMALOFT SILVER</p>
-                          <div className='sellViews-other'>
-                            <span className='pull-left'>库存: 219</span>
-                            <span className='pull-right'>交货周期: 3~5 天</span>
-                          </div>
-                      </div>
-                  </div>
-
-              </div>
+            <div className='row compareKeys'>
+                {this.renderViewsList()}
+            </div>
+            <div className='row compareValues'>
+                {this.renderViewItems()}
+            </div>
         </div>
       );
     }
 }
 
+//             <div className='pull-right sellViews-addView'>+ 添加视图</div>
+
 ReactDOM.render(
-    <SellViews />,
+    <SellViews views={['最吸引眼球','最热销','热点定制项']}/>,
     document.getElementById('body')
 );
